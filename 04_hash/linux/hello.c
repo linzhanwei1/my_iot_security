f943fe1fc6478a4efb2fa75f287ebde82b0ce74b
linzhanwei <linzhanwei1@163.com>
2022-09-28 16:44:22 +0800

    add tls

---
+++ 04_hash/linux/hello.c
@@ -0,0 +1,48 @@
+#if !defined(MBEDTLS_CONFIG_FILE)
+#include "mbedtls/config.h"
+#else
+#include MBEDTLS_CONFIG_FILE
+#endif
+
+#if defined(MBEDTLS_PLATFORM_C)
+#include "mbedtls/platform.h"
+#else
+#include <stdio.h>
+#define mbedtls_printf printf
+#endif
+
+#if defined(MBEDTLS_MD5_C)
+#include "mbedtls/md5.h"
+#endif
+
+#include <string.h>
+
+#if !defined(MBEDTLS_MD5_C)
+int main(void) {
+  mbedtls_printf("MBEDTLS_MD5_C not defined.\n");
+  return (0);
+}
+#else
+int main(void) {
+  int i;
+  unsigned char digest[16];
+  char str[] = "Hello, world!";
+
+  mbedtls_printf("\n MD5('%s') = ", str);
+  mbedtls_md5((unsigned char *)str, strlen(str), digest);
+
+  for (i = 0; i < 16; i++) {
+    mbedtls_printf("%02x", digest[i]);
+  }
+  mbedtls_printf("\n\n");
+
+#if defined(_WIN32)
+  mbedtls_printf(" Press Enter to exit this program.\n");
+  fflush(stdout);
+  getchar();
+#endif
+
+  return 0;
+}
+
+#endif /* MBEDTLS_MD5_C */
---
+++ 14_tls/tls/main/tls.c
@@ -0,0 +1,157 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/entropy.h"
+#include "mbedtls/net.h"
+#include "mbedtls/platform.h"
+#include "mbedtls/ssl.h"
+
+#include "esp_random.h"
+
+#include "ca_cert.h"
+
+#if defined(MBEDTLS_DEBUG_C)
+
+#include "mbedtls/debug.h"
+#define DEBUG_THRESHOLD 4
+
+static void my_debug(void *ctx, int level, const char *file, int line,
+                     const char *str) {
+  const char *p, *basename;
+
+  /* Extract basename from file */
+  for (p = basename = file; *p != '\0'; p++) {
+    if (*p == '/' || *p == '\\') {
+      basename = p + 1;
+    }
+  }
+
+  printf("%s:%04d: |%d| %s", basename, line, level, str);
+}
+
+#endif
+
+#define assert_exit(cond, ret)                                                 \
+  do {                                                                         \
+    if (!(cond)) {                                                             \
+      printf("   !. assert: failed [line: %d, error: -0x%04X]\n", __LINE__,    \
+             -(ret));                                                          \
+      goto cleanup;                                                            \
+    }                                                                          \
+  } while (0)
+
+#define SERVER_ADDR "182.92.148.82"
+#define SERVER_PORT "5200"
+#define HOSTNAME "182.92.148.82"
+#define GET_REQUEST "GET /index.html HTTP/1.0\r\n\r\n"
+
+static int entropy_source(void *data, uint8_t *output, size_t len,
+                          size_t *olen) {
+  uint32_t seed;
+  seed = esp_random();
+
+  if (len > sizeof(seed)) {
+    len = sizeof(seed);
+  }
+
+  memcpy(output, &seed, len);
+  *olen = len;
+
+  return 0;
+}
+
+void app_main(void) {
+  uint8_t buf[256];
+  int ret = 0, len = 0;
+  const char *pers = "tls_client";
+
+  mbedtls_entropy_context entropy;
+  mbedtls_ctr_drbg_context ctr_drbg;
+
+  mbedtls_x509_crt ca;
+  mbedtls_net_context ctx;
+  mbedtls_ssl_context ssl;
+  mbedtls_ssl_config conf;
+
+  mbedtls_net_init(&ctx);
+  mbedtls_ssl_init(&ssl);
+  mbedtls_ssl_config_init(&conf);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  mbedtls_x509_crt_init(&ca);
+
+  printf("\n  . Seeding the random number generator ...");
+
+  mbedtls_entropy_init(&entropy);
+  mbedtls_entropy_add_source(&entropy, entropy_source, NULL,
+                             MBEDTLS_ENTROPY_MAX_GATHER,
+                             MBEDTLS_ENTROPY_SOURCE_STRONG);
+  ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
+                              (const uint8_t *)pers, strlen(pers));
+  assert_exit(ret == 0, ret);
+
+  mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+  ret = mbedtls_x509_crt_parse_der(&ca, ca_cert_der, ca_cert_der_len);
+  assert_exit(ret == 0, ret);
+
+  mbedtls_ssl_conf_ca_chain(&conf, &ca, NULL);
+  mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_REQUIRED);
+
+#if defined(MBEDTLS_DEBUG_C)
+  mbedtls_debug_set_threshold(DEBUG_THRESHOLD);
+  mbedtls_ssl_conf_dbg(&conf, my_debug, NULL);
+#endif
+
+  ret = mbedtls_ssl_setup(&ssl, &conf);
+  assert_exit(ret == 0, ret);
+
+  ret = mbedtls_ssl_set_hostname(&ssl, HOSTNAME);
+  assert_exit(ret == 0, ret);
+
+  printf(" ok\n  . Connecting to %s:%s...", SERVER_ADDR, SERVER_PORT);
+  ret = mbedtls_net_connect(&ctx, SERVER_ADDR, SERVER_PORT,
+                            MBEDTLS_NET_PROTO_TCP);
+  assert_exit(ret == 0, ret);
+  mbedtls_ssl_set_bio(&ssl, &ctx, mbedtls_net_send, mbedtls_net_recv, NULL);
+
+  printf(" ok\n . Performing the SSL/TLS handshake ...");
+  while ((ret = mbedtls_ssl_handshake(&ssl)) != 0) {
+    if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+      printf(" failed\n  ! mbedtls_ssl_handshake returned -0x%x\n\n", -ret);
+      goto cleanup;
+    }
+  }
+
+  printf(" ok\n > Write to server:");
+  ret = mbedtls_ssl_write(&ssl, (const uint8_t *)GET_REQUEST,
+                          strlen(GET_REQUEST));
+  assert_exit(ret > 0, ret);
+
+  len = ret;
+  printf(" %d bytes written\n\n%s\n\n", len, GET_REQUEST);
+
+  printf(" > Read from server:");
+  len = sizeof(buf) - 1;
+  memset(buf, 0x00, sizeof(buf));
+
+  do {
+    ret = mbedtls_ssl_read(&ssl, buf, len);
+  } while (ret == MBEDTLS_ERR_SSL_WANT_WRITE ||
+           ret == MBEDTLS_ERR_SSL_WANT_READ);
+  assert_exit(ret > 0, ret);
+
+  len = ret;
+  printf(" %d bytes read \n\n\n%s\n\n", len, buf);
+
+  mbedtls_ssl_close_notify(&ssl);
+  printf(" ok\n . Closing the connection ... done\n");
+
+cleanup:
+  mbedtls_net_free(&ctx);
+  mbedtls_ssl_free(&ssl);
+  mbedtls_ssl_config_free(&conf);
+  mbedtls_ctr_drbg_free(&ctr_drbg);
+  mbedtls_entropy_free(&entropy);
+  mbedtls_x509_crt_free(&ca);
+}
